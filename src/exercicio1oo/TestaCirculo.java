1c7594acc1fb60283862a92281e4064af9081949
lucas <lucasdeoliveirapaza@gmail.com>
2025-04-14 20:29:38 -0300

    adicionar class Circulo e TestaCirculo

---
+++ src/exercicio1oo/TestaCirculo.java
@@ -0,0 +1,9 @@
+package exercicio1oo;
+
+class TestaCirculo {
+    public static void main(String args[]) {
+        Circulo bola = new Circulo();
+        bola.raio = 12.5;
+        System.out.println("raio:" + bola.raio);
+    }
+}
