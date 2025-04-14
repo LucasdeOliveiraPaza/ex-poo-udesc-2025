ea31c0b8dd5e397c2e0c73237967010ea8d95795
lucas <lucasdeoliveirapaza@gmail.com>
2025-04-14 20:34:22 -0300

    adicionar class Retangulo e TestaRetangulo

---
+++ src/exercicio1oo/TestaRetangulo.java
@@ -0,0 +1,13 @@
+package exercicio1oo;
+
+class TestaRetangulo {
+    public static void main(String args[]) {
+       Retangulo ret =  new Retangulo();
+
+       ret.altura = 23.5;
+       ret.largura = 14.6;
+
+       System.out.println("altura:" + ret.altura);
+       System.out.println("largura:" + ret.largura);
+    }
+}
