f1b10d5cba4a12838580811cc254eb0cbbc10f12
lucas <lucasdeoliveirapaza@gmail.com>
2025-05-03 09:48:29 -0300

    mudar classe contaBancaria para privado

---
+++ src/exercicio3oo/testes/TestaContaBancaria.java
@@ -0,0 +1,17 @@
+package exercicio3oo.testes;
+
+import exercicio3oo.classes.ContaBancaria;
+
+class TestaContaBancaria {
+    public static void main(String args[]) {
+        ContaBancaria cb = new ContaBancaria();
+
+        cb.setNumeroConta("1123");
+        cb.setTitular("Lucas");
+        cb.setSaldo(8597.22);
+
+        System.out.println("numero conta:" + cb.getNumeroConta());
+        System.out.println("titular:" + cb.getTitular());
+        System.out.println("saldo:" + cb.getSaldo());
+    }
+}
