bff0553eeddb46104f04c6d8da433e7945017b14
lucas <lucasdeoliveirapaza@gmail.com>
2025-05-14 21:23:45 -0300

    implementar construtor to string e validacoes no aluno

--- src/exercicio5oo/classes/Aluno.java
+++ src/exercicio5oo/classes/Aluno.java
@@ -6,6 +6,21 @@ public class Aluno {
     private int idade;
     private double nota1, nota2, nota3, nota4;
 
+    public Aluno () {
+        this.matricula = "";
+        this.nome = "";
+    }
+
+    public Aluno (String matricula, String nome, int idade, double nota1, double nota2, double nota3, double nota4) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.idade = idade;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+        this.nota4 = nota4;
+    }
+
     public String getMatricula() {
         return matricula;
     }
@@ -35,7 +50,9 @@ public class Aluno {
     }
 
     public void setNota1(double nota1) {
-        this.nota1 = nota1;
+        if (this.notaValida(nota1)) {
+            this.nota1 = nota1;
+        }
     }
 
     public double getNota2() {
@@ -43,7 +60,9 @@ public class Aluno {
     }
 
     public void setNota2(double nota2) {
-        this.nota2 = nota2;
+        if (this.notaValida(nota2)) {
+            this.nota2 = nota2;
+        }
     }
 
     public double getNota3() {
@@ -51,7 +70,9 @@ public class Aluno {
     }
 
     public void setNota3(double nota3) {
-        this.nota3 = nota3;
+        if (this.notaValida(nota3)) {
+            this.nota3 = nota3;
+        }
     }
 
     public double getNota4() {
@@ -59,7 +80,9 @@ public class Aluno {
     }
 
     public void setNota4(double nota4) {
-        this.nota4 = nota4;
+        if (this.notaValida(nota4)) {
+            this.nota4 = nota4;
+        }
     }
 
     public double calcularMedia() {
@@ -72,5 +95,26 @@ public class Aluno {
         } else {
             return "Aprovado";
         }
+    }
+
+    private boolean notaValida(double nota) {
+        if (nota > 10 || nota < 0) {
+            System.out.println("Nota Invalida será considerado como zero");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Objeto Aluno ["+
+            "\n\tmatricula = " + matricula +
+            "\n\tnome = " + nome +
+            "\n\tidade = " + idade +
+            "\n\tnota1 = " + nota1 +
+            "\n\tnota2 = " + nota2 +
+            "\n\tnota3 = " + nota3 +
+            "\n\tnota4 = " + nota4 +
+        "\n]";
     }
 }
--- src/exercicio5oo/testes/TestaAluno.java
+++ src/exercicio5oo/testes/TestaAluno.java
@@ -4,17 +4,32 @@ import exercicio5oo.classes.Aluno;
 
 class TestaAluno {
     public static void main(String args[]) {
-        Aluno lucas = new Aluno();
+        Aluno lucas = new Aluno(
+            "22123123214",
+            "Lucas de Oliveira Paza",
+            22,
+            10,
+            7,
+            8,
+            9.5
+        );
 
-        lucas.setNome("Lucas de Oliveira Paza");
-        lucas.setIdade(22);
-        lucas.setMatricula("22123123214");
-        lucas.setNota1(10);
-        lucas.setNota2(7);
-        lucas.setNota3(7);
-        lucas.setNota4(6);
-
+        System.out.println(lucas);
         System.out.println("media: " + lucas.calcularMedia());
         System.out.println("situacao: " + lucas.buscarSituacao());
+
+        Aluno fulano = new Aluno();
+
+        fulano.setNome("Fulano Fuleiro da Funileira");
+        fulano.setMatricula("40028922");
+        fulano.setIdade(92);
+        fulano.setNota1(11);
+        fulano.setNota2(-17);
+        fulano.setNota3(6);
+        fulano.setNota4(10);
+
+        System.out.println(fulano);
+        System.out.println("media: " + fulano.calcularMedia());
+        System.out.println("situacao: " + fulano.buscarSituacao());
     }
 }
